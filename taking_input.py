da818cf20f48b343354e33f21fd7226937246e93
thrivediraju <104252421+thrivediraju@users.noreply.github.com>
2022-06-06 11:16:05 +0530

    Add files via upload

---
+++ taking_input.py
@@ -0,0 +1,14 @@
+# a=5 ; b=5
+# print(a+b)
+
+s1=input('Enter the Value for S1:')
+s2=input('Entet the Value for S2:')
+print('Concatenation',s1+s2)
+
+s1=int(input('Enter the Value for S1:'))
+s2=int(input('Entet the Value for S2:'))
+print('Addition with int is',s1+s2)
+
+s1=float(input('Enter the Value for S1:'))
+s2=float(input('Entet the Value for S2:'))
+print('Addition with float is',s1+s2)
